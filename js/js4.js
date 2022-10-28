fc2ac3811f0ef2f3ad5f5dd95ebe3a587c836c3c
omarlozano-02 <omarlozano771@gmail.com>
2022-10-28 01:19:36 -0500

    first commit

---
+++ js/js4.js
@@ -0,0 +1,6 @@
+horaInicial = Number(prompt("Ingrese la hora inicial (0-23):"));
+
+frecuencia = Number(prompt("Ingrese la frecuencia (1-12):"));
+
+document.write("Hora incial: ", horaInicial);
+document.write("<br/>Frecuencia: ", frecuencia);
